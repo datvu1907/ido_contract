a6a2976ec15118232fd0fa0c9a09a9cfcf6036b9
Leon Nguyen <vicnguyenvuong@gmail.com>
2022-05-21 16:12:42 +0700

    add test file

--- contracts/IDO_Contract.sol
+++ contracts/IDO_Contract.sol
@@ -4,6 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "hardhat/console.sol";
 contract IDO is ERC20, Ownable{
     using SafeMath for uint256;
     struct user{
@@ -23,6 +24,10 @@ contract IDO is ERC20, Ownable{
     uint256 public totalToken;
     uint256 SWAP_RATE = 1000; // 1 ether = 1000 token
     
+    event JoinWhiteList(address indexed user, uint256 amount);
+    event Vesting(address owner, uint256 amount);
+    event Claim(address indexed user, uint256 amount, uint256 yearGoneBy, uint256 monthGoneBy);
+
     constructor() ERC20("Near Protocol", "NEAR2"){
         _mint(msg.sender, 2000 * 10**18);
         Token = IERC20(address(this));
@@ -45,47 +50,63 @@ contract IDO is ERC20, Ownable{
         listVesting[_msgSender()].amount = amount;
         listVesting[_msgSender()].userClaim = 0;
         totalToken.trySub(amount);
+        emit JoinWhiteList(_msgSender(), amount);
     }
 
     function fundVesting() public onlyOwner{
         transfer(address(this), totalToken * 10**18);
+        emit Vesting(_msgSender(), totalToken);
     }
 
     function claim() public  {
-        uint256 YearsGoneBy = ((block.timestamp - starTime)/ 31536000);
-        uint256 MonthsGoneBy = ((block.timestamp - starTime)% 31536000);
+        uint256 YearsGoneBy = ((block.timestamp - starTime)/ 12);   // 1 year = 24 second for test
+        uint256 MonthsGoneBy = ((block.timestamp - starTime)% 12);
         require(listVesting[_msgSender()].amount > 0, "You are not in vesting");
-    
-        if( YearsGoneBy < cliff){      /// first claim
+        require(listVesting[_msgSender()].userClaim <= listVesting[_msgSender()].amount, "You have claimed all");
+        console.log("YearsGoneBy %s and MonthsGoneBuy ", YearsGoneBy, MonthsGoneBy);
+        if( YearsGoneBy < 1){      /// first claim
             // check first claim of user 
+        
             uint256 firstClaim = listVesting[_msgSender()].amount * 20/100;  
-            require(listVesting[_msgSender()].userClaim <= firstClaim, "You have claimed in this year");
+            require(listVesting[_msgSender()].userClaim < 200, "You have claimed in this year");
 
             Token.transfer(_msgSender(), firstClaim * 10**18);
             listVesting[_msgSender()].userClaim = listVesting[_msgSender()].amount * 20/100;
-        }else{
-            if(YearsGoneBy > 1 && MonthsGoneBy >= totalPeriods || YearsGoneBy >= 2){ // claim all after cliff
-
+            emit Claim(_msgSender(), firstClaim, YearsGoneBy, MonthsGoneBy);
+        }else{ // claim after cliff
+        
+            if(YearsGoneBy == 1 && MonthsGoneBy >= totalPeriods || YearsGoneBy >= 2){ 
+             /**?
+                Claim all for after period
+                 */
             // Check if user have claim all
-            require(listVesting[_msgSender()].userClaim <= listVesting[_msgSender()].amount, "You have claimed all token");
-            uint256 tokenLeft =  listVesting[_msgSender()].amount - listVesting[_msgSender()].amount;
-
-
+            require(listVesting[_msgSender()].userClaim < listVesting[_msgSender()].amount, "You have claimed all token");
+            uint256 tokenLeft =  listVesting[_msgSender()].amount - listVesting[_msgSender()].userClaim;
+            
+            console.log("tokenLeft %s", tokenLeft);  
             Token.transfer(_msgSender(),  tokenLeft * 10**18);
             listVesting[_msgSender()].userClaim = listVesting[_msgSender()].amount;
-            }else{
+            emit Claim(_msgSender(), tokenLeft, YearsGoneBy, MonthsGoneBy);
+
+            }else{      
+                /**?
+                Claim for each period
+                 */
             // total token in this month
             uint256 totalClaim = listVesting[_msgSender()].amount * 20/100 + listVesting[_msgSender()].amount* MonthsGoneBy * 10/100; 
-            
+            console.log("totalClaim %s", totalClaim);
             // total token that user can claim in this month
             uint256 thisMonthClaim =  totalClaim - listVesting[_msgSender()].userClaim;
-
+        
+            console.log("thisMonthClaim %s", thisMonthClaim);
             //check if user claim have claim in this month or not
-            require(listVesting[_msgSender()].userClaim <= totalClaim, "You have claimed in this month"); 
+            require(listVesting[_msgSender()].userClaim < totalClaim, "You have claimed in this month"); 
 
             
             Token.transfer(_msgSender(), thisMonthClaim * 10**18);
-            listVesting[_msgSender()].userClaim.tryAdd(thisMonthClaim);
+            listVesting[_msgSender()].userClaim += thisMonthClaim;
+            console.log("userClaim %s", listVesting[_msgSender()].userClaim);
+            emit Claim(_msgSender(), thisMonthClaim, YearsGoneBy, MonthsGoneBy);
 
             }
            
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,161 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+let hardhatToken;
+let owner;
+let addr1;
+let addr2;
+let addrs;
+let Token;
+var MILISECOND = 1000; // 1 cliff = 12s
+beforeEach(async () => {
+  myToken = await ethers.getContractFactory("IDO");
+  [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+  hardhatToken = await myToken.deploy();
+});
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("Token contract", function () {
+  it("Deployment should assign the total supply of tokens to the owner", async function () {
+    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+  });
+});
+describe("Funding Transaction", function () {
+  it("Check owner blance after vesting", async function () {
+    await hardhatToken.connect(owner).fundVesting();
+    const ownerBalanceAfter = await hardhatToken.balanceOf(owner.address);
+    expect(ownerBalanceAfter).to.equal(0);
+  });
+});
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+describe("Single Transaction", function () {
+  it("User 1 buy 1000 Token and first claim", async function () {
+    await hardhatToken
+      .connect(addr1)
+      .joinWhiteList(1000, { value: ethers.utils.parseEther("1.0") });
+    await hardhatToken.connect(owner).fundVesting();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    // await new Promise((resolve) => setTimeout(resolve, MILISECOND));
+    await hardhatToken.connect(addr1).claim();
+    const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+    expect(addr1Balance).to.equal(
+      ethers.BigNumber.from("200000000000000000000")
+    );
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    // expect(ownerBalanceAfter).to.equal(0);
+  });
+  it("User 1 buy 1000 Token and first claim and second claim is 4 month after cliff", async function () {
+    await hardhatToken
+      .connect(addr1)
+      .joinWhiteList(1000, { value: ethers.utils.parseEther("1.0") });
+    await hardhatToken.connect(owner).fundVesting();
+
+    await hardhatToken.connect(addr1).claim();
+    await new Promise((resolve) => setTimeout(resolve, 24000));
+    await hardhatToken.connect(addr1).claim();
+    const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+
+    expect(addr1Balance).to.equal(
+      ethers.BigNumber.from("600000000000000000000")
+    );
+  });
+  it("User 1 buy 1000 Token and first claim and second claim and third claim 10%", async function () {
+    await hardhatToken
+      .connect(addr1)
+      .joinWhiteList(1000, { value: ethers.utils.parseEther("1.0") });
+    await hardhatToken.connect(owner).fundVesting();
+    // first claim
+    await hardhatToken.connect(addr1).claim();
+
+    // second claim
+    await new Promise((resolve) => setTimeout(resolve, 25000));
+    await hardhatToken.connect(addr1).claim();
+
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // third claim
+    await hardhatToken.connect(addr1).claim();
+    const addr1Balance2 = await hardhatToken.balanceOf(addr1.address);
+    expect(addr1Balance2).to.equal(
+      ethers.BigNumber.from("700000000000000000000")
+    );
+
+    // expect(ownerBalanceAfter).to.equal(0);
+  });
+  it("User 1 buy 1000 Token and first claim and second claim and third claim all%", async function () {
+    await hardhatToken
+      .connect(addr1)
+      .joinWhiteList(1000, { value: ethers.utils.parseEther("1.0") });
+    await hardhatToken.connect(owner).fundVesting();
+    // first claim
+    await hardhatToken.connect(addr1).claim();
+
+    // second claim
+    await new Promise((resolve) => setTimeout(resolve, 12000));
+    await hardhatToken.connect(addr1).claim();
+
+    // third claim
+    await new Promise((resolve) => setTimeout(resolve, 5000)); // third claim
+    await hardhatToken.connect(addr1).claim();
+    const addr1Balance2 = await hardhatToken.balanceOf(addr1.address);
+    expect(addr1Balance2).to.equal(
+      ethers.BigNumber.from("1000000000000000000000")
+    );
+
+    // expect(ownerBalanceAfter).to.equal(0);
+  });
+
+  it("User 1 buy 2000 Token and fail", async function () {
+    await hardhatToken
+      .connect(addr1)
+      .joinWhiteList(2000, { value: ethers.utils.parseEther("2.0") });
+
+    // const ownerBalanceAfter = await hardhatToken.balanceOf(owner.address);
+    // expect(ownerBalanceAfter).to.equal(0);
+  });
+});
+
+describe("more than 1 transaction", function () {
+  it("User 1 buy 1000 Token and user 2 buy 1000 Token and claim", async function () {
+    await hardhatToken
+      .connect(addr1)
+      .joinWhiteList(1000, { value: ethers.utils.parseEther("1.0") });
+    await hardhatToken
+      .connect(addr2)
+      .joinWhiteList(1000, { value: ethers.utils.parseEther("1.0") });
+
+    await hardhatToken.connect(owner).fundVesting();
+    // first claim
+    await hardhatToken.connect(addr1).claim();
+
+    // second claim
+    await new Promise((resolve) => setTimeout(resolve, 12000));
+    await hardhatToken.connect(addr1).claim();
+
+    // third claim
+    await new Promise((resolve) => setTimeout(resolve, 5000)); // third claim
+    await hardhatToken.connect(addr1).claim();
+    const addr1Balance2 = await hardhatToken.balanceOf(addr1.address);
+    expect(addr1Balance2).to.equal(
+      ethers.BigNumber.from("1000000000000000000000")
+    );
+    await hardhatToken.connect(addr2).claim();
+    const addr2Balance = await hardhatToken.balanceOf(addr1.address);
+    expect(addr2Balance).to.equal(
+      ethers.BigNumber.from("1000000000000000000000")
+    );
+  });
+
+  it(" fail when User 1 buy 1000 Token and user 2 buy 1000 Token and user 3 buy 1000 Token", async function () {
+    await hardhatToken
+      .connect(addr1)
+      .joinWhiteList(1000, { value: ethers.utils.parseEther("1.0") });
+    await hardhatToken
+      .connect(addr2)
+      .joinWhiteList(1000, { value: ethers.utils.parseEther("1.0") });
+    await hardhatToken
+      .connect(addr3)
+      .joinWhiteList(1000, { value: ethers.utils.parseEther("1.0") });
+    await hardhatToken.connect(owner).fundVesting();
+    // first claim
+    await hardhatToken.connect(addr1).claim();
   });
 });
